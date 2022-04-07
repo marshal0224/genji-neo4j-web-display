1948ea7c611b5037106f45866cf7203e990ac018
Yuhao Dong <dyh@bu.edu>
2022-04-06 21:08:49 -0400

    found rsuite and gave it a shot in filter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'rsuite/dist/rsuite.min.css';
 
 const container = document.getElementById('root');
 
