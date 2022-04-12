d8016a5ac7aa949be546c96f9d6bcef9ef8e5682
Yuhao Dong <dyh@bu.edu>
2022-04-12 17:26:34 -0400

    implemented a likely buggy first draft of speaker-addressee filter links

--- src/components/poem.js
+++ src/components/poem.js
@@ -20,13 +20,42 @@ export default class Poem extends React.Component {
         const chapter = this.props.chapter
         const speaker = this.props.speaker
         const addressee = this.props.addressee
-
-        const getPoem = `match 
-                            (s:Character {name: $speaker})-[p: SPEAKER_OF]-(j:Japanese)-[q:ADDRESSEE_OF]-(a:Character {name: $addressee}), 
-                            (j:Japanese)-[r:INCLUDED_IN]-(c:Chapter {chapter_number: $chapter})
-                        return (j) as poems`
+        console.log('chapter for poem table is: '+chapter)
         
         try {
+            let getPoem_S, getPoem_A, getPoem_C
+            if (speaker === 'Any') {
+                getPoem_S = '(s:Character)'
+            } else {
+                getPoem_S = '(s:Character {name: "'+speaker+'"})'
+            } 
+            if (addressee === 'Any') {
+                getPoem_A = '(a:Character)'
+            } else {
+                getPoem_A = '(a:Character {name: "'+addressee+'"})'
+            }
+            if (chapter === 'Any') {
+                getPoem_C = ''
+            } else {
+                //as of Apirl 2022, the chapter numbers are in string
+                getPoem_C = ', (j)-[r:INCLUDED_IN]-(c:Chapter {chapter_number: "'+chapter+'"})'
+            }
+            let getPoem =   'match '+getPoem_S+'-[p: SPEAKER_OF]-(j:Japanese)-'
+                            +'[q:ADDRESSEE_OF]-'+getPoem_A 
+                            +getPoem_C
+                            +' return (j) as poems'
+            // if (chapter === 'Any') {
+            //     getPoem = `match 
+            //                         (s:Character {name: $speaker})-[p: SPEAKER_OF]-(j:Japanese)-[q:ADDRESSEE_OF]-(a:Character {name: $addressee}), 
+            //                         (j:Japanese)-[r:INCLUDED_IN]-(c:Chapter {chapter_number: $chapter})
+            //                     return (j) as poems`
+            // } else {
+            //     getPoem = `match 
+            //                         (s:Character)-[p: SPEAKER_OF]-(j:Japanese)-[q:ADDRESSEE_OF]-(a:Character {name: $addressee}), 
+            //                         (j:Japanese)-[r:INCLUDED_IN]-(c:Chapter {chapter_number: $chapter})
+            //                     return (j) as poems`
+            // }
+            console.log(getPoem)
             const res = await session.readTransaction(tx => tx.run(getPoem, { speaker, addressee, chapter}))
             let temp = res.records.map(row => {return toNativeTypes(row.get('poems'))})
             // properties[i]: {Japanese, Romaji, pnum}
@@ -44,9 +73,7 @@ export default class Poem extends React.Component {
         closeDriver()
     }
 
-    async componentDidUpdate(nextState) {
-        console.log('Poem did update')
-    }
+    getDerived
 
     render() {
         let properties = this.state.poemProperties;
