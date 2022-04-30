b58ee7141025103647b1bae7b309ccbe6de36291
Yuhao Dong <dyh@bu.edu>
2022-04-29 21:06:33 -0400

    fixed filter and poem table compatability issue to reflect Neo4j changes made while importing Uji chapter data.  Also added the chapter order in the first poem table column.

--- src/components/poem.js
+++ src/components/poem.js
@@ -17,175 +17,176 @@ export default class Poem extends React.Component {
 
     parsePnum(pnum) {
         let [chp, _, order] = pnum.match(/.{1,2}/g)
+        let chp_name
         switch(chp){
             case '01':
-                chp = "Kiritsubo 桐壺"
+                chp_name = "Kiritsubo 桐壺"
                 break
             case '02':
-                chp = "Hahakigi 帚木"
+                chp_name = "Hahakigi 帚木"
                 break
             case '03': 
-                chp = "Utsusemi 空蝉"
+                chp_name = "Utsusemi 空蝉"
                 break
             case '04': 
-                chp = "Yūgao 夕顔"
+                chp_name = "Yūgao 夕顔"
                 break
             case '05': 
-                chp = "Wakamurasaki 若紫"
+                chp_name = "Wakamurasaki 若紫"
                 break
             case '06': 
-                chp = "Suetsumuhana 末摘花"
+                chp_name = "Suetsumuhana 末摘花"
                 break
             case '07': 
-                chp = "Momiji no Ga 紅葉賀"
+                chp_name = "Momiji no Ga 紅葉賀"
                 break
             case '08': 
-                chp = "Hana no En 花宴"
+                chp_name = "Hana no En 花宴"
                 break
             case '09': 
-                chp = "Aoi 葵"
+                chp_name = "Aoi 葵"
                 break
             case '10': 
-                chp = "Sakaki 榊"
+                chp_name = "Sakaki 榊"
                 break
             case '11': 
-                chp = "Hana Chiru Sato 花散里"
+                chp_name = "Hana Chiru Sato 花散里"
                 break
             case '12': 
-                chp = "Suma 須磨"
+                chp_name = "Suma 須磨"
                 break
             case '13': 
-                chp = "Akashi 明石"
+                chp_name = "Akashi 明石"
                 break
             case '14': 
-                chp = "Miotsukushi 澪標"
+                chp_name = "Miotsukushi 澪標"
                 break
             case '15': 
-                chp = "Yomogiu 蓬生"
+                chp_name = "Yomogiu 蓬生"
                 break
             case '16': 
-                chp = "Sekiya 関屋"
+                chp_name = "Sekiya 関屋"
                 break
             case '17': 
-                chp = "E Awase 絵合"
+                chp_name = "E Awase 絵合"
                 break
             case '18': 
-                chp = "Matsukaze 松風"
+                chp_name = "Matsukaze 松風"
                 break
             case '19': 
-                chp = "Usugumo 薄雲"
+                chp_name = "Usugumo 薄雲"
                 break
             case '20': 
-                chp = "Asagao 朝顔"
+                chp_name = "Asagao 朝顔"
                 break
             case '21': 
-                chp = "Otome 乙女"
+                chp_name = "Otome 乙女"
                 break
             case '22': 
-                chp = "Tamakazura 玉鬘"
+                chp_name = "Tamakazura 玉鬘"
                 break
             case '23': 
-                chp = "Hatsune 初音"
+                chp_name = "Hatsune 初音"
                 break
             case '24': 
-                chp = "Kochō 胡蝶"
+                chp_name = "Kochō 胡蝶"
                 break
             case '25': 
-                chp = "Hotaru 螢"
+                chp_name = "Hotaru 螢"
                 break
             case '26': 
-                chp = "Tokonatsu 常夏"
+                chp_name = "Tokonatsu 常夏"
                 break
             case '27': 
-                chp = "Kagaribi 篝火"
+                chp_name = "Kagaribi 篝火"
                 break
             case '28': 
-                chp = "Nowaki 野分"
+                chp_name = "Nowaki 野分"
                 break
             case '29': 
-                chp = "Miyuki 行幸"
+                chp_name = "Miyuki 行幸"
                 break
             case '30': 
-                chp = "Fujibakama 藤袴"
+                chp_name = "Fujibakama 藤袴"
                 break
             case '31': 
-                chp = "Makibashira 真木柱"
+                chp_name = "Makibashira 真木柱"
                 break
             case '32': 
-                chp = "Umegae 梅枝"
+                chp_name = "Umegae 梅枝"
                 break
             case '33': 
-                chp = "Fuji no Uraba 藤裏葉"
+                chp_name = "Fuji no Uraba 藤裏葉"
                 break
             case '34': 
-                chp = "Wakana: Jō 若菜上"
+                chp_name = "Wakana: Jō 若菜上"
                 break
             case '35': 
-                chp = "Wakana: Ge 若菜下"
+                chp_name = "Wakana: Ge 若菜下"
                 break
             case '36': 
-                chp = "Kashiwagi 柏木"
+                chp_name = "Kashiwagi 柏木"
                 break
             case '37': 
-                chp = "Yokobue 横笛"
+                chp_name = "Yokobue 横笛"
                 break
             case '38': 
-                chp = "Suzumushi 鈴虫"
+                chp_name = "Suzumushi 鈴虫"
                 break
             case '39':
-                chp = "Yūgiri 夕霧"
+                chp_name = "Yūgiri 夕霧"
                 break
             case '40': 
-                chp = "Minori 御法"
+                chp_name = "Minori 御法"
                 break
             case '41': 
-                chp = "Maboroshi 幻"
+                chp_name = "Maboroshi 幻"
                 break
             case '42': 
-                chp = "Niō Miya 匂宮"
+                chp_name = "Niō Miya 匂宮"
                 break
             case '43': 
-                chp = "Kōbai 紅梅"
+                chp_name = "Kōbai 紅梅"
                 break
             case '44': 
-                chp = "Takekawa 竹河"
+                chp_name = "Takekawa 竹河"
                 break
             case '45': 
-                chp = "Hashihime 橋姫"
+                chp_name = "Hashihime 橋姫"
                 break
             case '46': 
-                chp = "Shii ga Moto 椎本"
+                chp_name = "Shii ga Moto 椎本"
                 break
             case '47': 
-                chp = "Agemaki 総角"
+                chp_name = "Agemaki 総角"
                 break
             case '48': 
-                chp = "Sawarabi 早蕨"
+                chp_name = "Sawarabi 早蕨"
                 break
             case '49': 
-                chp = "Yadorigi 宿木"
+                chp_name = "Yadorigi 宿木"
                 break
             case '50': 
-                chp = "Azumaya 東屋"
+                chp_name = "Azumaya 東屋"
                 break
             case '51':
-                chp = "Ukifune 浮舟"
+                chp_name = "Ukifune 浮舟"
                 break
             case '52':
-                chp = "Kagerō 蜻蛉"
+                chp_name = "Kagerō 蜻蛉"
                 break
             case '53':
-                chp = "Tenarai 手習"
+                chp_name = "Tenarai 手習"
                 break
             case '54':
-                chp = "Yume no Ukihashi 夢浮橋"
+                chp_name = "Yume no Ukihashi 夢浮橋"
                 break
             default: 
                 console.log('unknown chapter caught')
         }
         order = parseInt(order)
         return (
-            <p>{chp} {order}</p>
+            <p>{chp} {chp_name} {order}</p>
         )
     }
 
@@ -208,17 +209,18 @@ export default class Poem extends React.Component {
                 getAddressee = '(a:Character {name: "'+addressee+'"})'
             }
             if (chapter === 'Any') {
-                getChapter = ', (j)-[r:INCLUDED_IN]-(c:Chapter), '
+                getChapter = ', (g)-[r:INCLUDED_IN]-(c:Chapter), '
             } else {
                 //as of Apirl 2022, the chapter numbers are in string
-                getChapter = ', (j)-[r:INCLUDED_IN]-(c:Chapter {chapter_number: "'+chapter+'"}), '
+                getChapter = ', (g)-[r:INCLUDED_IN]-(c:Chapter {chapter_number: "'+chapter+'"}), '
             }
-            let get =   'match exchange='+getSpeaker+'-[p:SPEAKER_OF]-(j:Japanese)-'
+            let get =   'match exchange='+getSpeaker+'-[p:SPEAKER_OF]-(g:Genji_Poem)-'
                             +'[q:ADDRESSEE_OF]-'+getAddressee 
                             +getChapter
-                            +'trans=(j)-[u:TRANSLATION_OF]-(t:Translation)'
+                            +'trans=(g)-[u:TRANSLATION_OF]-(t:Translation)'
                             +' return exchange, trans'
             const res = await session.readTransaction(tx => tx.run(get, { speaker, addressee, chapter}))
+            // console.log(get)
             let poemRes = res.records.map(row => {return toNativeTypes(row.get('exchange'))})
             let transRes = res.records.map(row => {return toNativeTypes(row.get('trans'))})
             let Japanese = poemRes.map(row => row.segments[1].start.properties)
@@ -250,12 +252,17 @@ export default class Poem extends React.Component {
                     } else {
                         count = 1
                     }
+                    let pnum_count = count
                     if (Translation[element[count+1].substring(0,6)] === undefined) {
                         Translation[element[count+1].substring(0,6)] = {}
                     }
                     let auth = element[count+1].substring(6,7)
                     if (auth === 'A') {
                         auth = 'Waley'
+                        // special case handler: Waley trans with a page number
+                        if (element.length === 3) {
+                            pnum_count -= 1
+                        }
                     } else if (auth === 'C') {
                         auth = 'Cranston'
                     } else if (auth === 'S') {
@@ -266,7 +273,7 @@ export default class Poem extends React.Component {
                         auth = 'Washburn'
                     }
                     // console.log(element[count+1].substring(0,6))
-                    Translation[element[count+1].substring(0,6)][auth] = element[count]
+                    Translation[element[count+1].substring(0,6)][auth] = element[pnum_count]
                 }
             });
             this.setState({
