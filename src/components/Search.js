1ccc2b16adb922d1a3a7bfcdd7da71c966d45199
Yuhao Dong <1243676996@qq.com>
2022-08-16 23:04:54 -0400

    started using Antd

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { initDriver, getDriver, closeDriver } from '../neo4j'
-import { toNativeTypes } from '../utils'
+import { toNativeTypes, getChpList } from '../utils'
+import { Select } from 'antd';
+import 'antd/dist/antd.css';
 import _ from 'lodash'
+// const { Option } = Select;
 
 export default class Search extends React.Component {
     Graph = require("graph-data-structure")
@@ -9,9 +12,9 @@ export default class Search extends React.Component {
         super(props)
         this.state = {
             // original data pulled from Neo4j
-            chapters: [],
+            chapters: Array.from(Array(54), (_,i)=> i),
             characters: [],
-            charNum: 0,
+            // charNum: 0,
             speakers: [],
             addressees: [],
             genders: [], 
@@ -31,6 +34,13 @@ export default class Search extends React.Component {
         this.user = process.env.REACT_APP_NEO4J_USERNAME
         this.password = process.env.REACT_APP_NEO4J_PASSWORD
         this.resetFilters = this.resetFilters.bind(this)
+        this.hasNode = this.hasNode.bind(this)
+    }
+
+    hasNode(graph, node) {
+        let prev = JSON.stringify(graph.nodes())
+        let after = JSON.stringify(graph.addNode(node).nodes())
+        return prev === after
     }
 
     async componentDidMount() {
@@ -38,29 +48,65 @@ export default class Search extends React.Component {
         const driver = getDriver()
         const session = driver.session()
         console.log('40')
-        let query = 'match (g:Genji_Poem), exchange=(s:Character)-[:SPEAKER_OF]-(g:Genji_Poem)-[:ADDRESSEE_OF]-(a:Character) return g.pnum as pnum, exchange'
+        let query = 'match exchange=(s:Character)-[:SPEAKER_OF]-(g:Genji_Poem)-[:ADDRESSEE_OF]-(a:Character) return exchange'
         let res = await session.readTransaction(tx => tx.run(query))
-        let pnum = res.records.map(row => {return toNativeTypes(row.get('pnum'))})
-        // chp_poem = chp_poem.map(row => [row[0].start.properties.chapter_number, row[0].end.properties.pnum])
         let exchange = res.records.map(row => {return toNativeTypes(row.get('exchange'))})
-        console.log(chp_poem)
+        let graph = this.Graph()
+        let characters = []
+        let speakers = []
+        let addressees = []
+        this.state.addresseeGenderList.forEach(gender => graph.addNode(gender))
+        for (let i = 0; i < exchange.length; i++) {
+            let pnum = exchange[i].segments[0].end.properties.pnum
+            let spkr = exchange[i].start.properties.name
+            let spkr_gen = exchange[i].start.properties.gender
+            let addr = exchange[i].end.properties.name
+            let addr_gen = exchange[i].end.properties.gender
+            if (!this.hasNode(graph, spkr)) {
+                characters.push(spkr)
+                graph.addEdge(spkr_gen, spkr)
+            }
+            if (!this.hasNode(graph, addr)) {
+                characters.push(addr)
+                graph.addEdge(addr_gen, spkr)
+            }
+            if (!graph.hasEdge(pnum, pnum.substring(0,2))) {
+                graph.addEdge(pnum, pnum.substring(0,2))
+            }
+            // w=2 for speaker, w=1 for addressee
+            if (!graph.hasEdge(spkr, pnum, 2)) {
+                graph.addEdge(spkr, pnum, 2)
+            }
+            graph.addEdge(addr, pnum, 1)
+        }
+        for (let i = 0; i < characters.length; i++) {
+            let add_list = graph.adjacent(characters[i]).filter(n => graph.getEdgeWeight(characters[i], n) === 1)
+            if (add_list.length > 0) {
+                addressees.push(characters[i])
+            }
+            let spk_list = graph.adjacent(characters[i]).filter(n => graph.getEdgeWeight(characters[i], n) === 2)
+            if (spk_list.length > 0) {
+                speakers.push(characters[i])
+            }
+        }
+        // console.log(addressees.sort())
         session.close()
         closeDriver()
     }
 
-    async componentDidUpdate() {
-        initDriver(this.uri, this.user, this.password)
-        const driver = getDriver()
-        const session = driver.session()
-        console.log('53')
-        let query = 'match chp_poem=(:Chapter)-[:INCLUDED_IN]-(:Genji_Poem), exchange=(:Character)-[:SPEAKER_OF]-(:Genji_Poem)-[:ADDRESSEE_OF]-(:Character) return chp_poem, exchange'
-        let res = await session.readTransaction(tx => tx.run(query))
-        let chp_poem = res.records.map(row => {return toNativeTypes(row.get('chp_poem'))})
-        let exchange = res.records.map(row => {return toNativeTypes(row.get('exchange'))})
-        console.log(chp_poem)
-        session.close()
-        closeDriver()
-    }
+    // async componentDidUpdate() {
+    //     initDriver(this.uri, this.user, this.password)
+    //     const driver = getDriver()
+    //     const session = driver.session()
+    //     console.log('53')
+    //     let query = 'match chp_poem=(:Chapter)-[:INCLUDED_IN]-(:Genji_Poem), exchange=(:Character)-[:SPEAKER_OF]-(:Genji_Poem)-[:ADDRESSEE_OF]-(:Character) return chp_poem, exchange'
+    //     let res = await session.readTransaction(tx => tx.run(query))
+    //     let chp_poem = res.records.map(row => {return toNativeTypes(row.get('chp_poem'))})
+    //     let exchange = res.records.map(row => {return toNativeTypes(row.get('exchange'))})
+    //     console.log(chp_poem)
+    //     session.close()
+    //     closeDriver()
+    // }
 
     resetFilters = (event) => {
         let backup = this.state.backup
@@ -87,30 +133,24 @@ export default class Search extends React.Component {
     }
 
     render() {
-        const chpGen = function(list, sg, ag, chapter, exchange, genders, characters) {
-            if (sg === 'Any' && ag === 'Any') {
-                list.push([...chapter, 1])
-            } else if (sg === 'Any' && genders[characters.indexOf(exchange[1])] === ag) {
-                list.push([...chapter, 1])
-            } else if (ag === 'Any' && genders[characters.indexOf(exchange[0])] === sg) {
-                list.push([...chapter, 1])
-            } else if (genders[characters.indexOf(exchange[0])] === sg && genders[characters.indexOf(exchange[1])] === ag) {
-                list.push([...chapter, 1])
-            } 
-            return list
-        }
         return (
             <div>
-                <p>Hello world</p>
-                {/* <form>
+                {/* <p>Hello world</p> */}
+                <form>
                     <label htmlFor="chapter">Chapter</label>
                     <br />
-                    <select id="chapter" value={this.state.selectedChapter} onChange={updateSelection} name="chapter">
-                        <option value="Any">Any</option>
-                        {this.state.chapterList.filter(row => row[row.length-1]).map((row) => <option key={row[0]} value={row[0]}>{row[0]+' '+row[1]+' '+row[2]}</option>)}
-                    </select>
+                    <Select 
+                        style={{ width:220 }}
+                        showSearch
+                        placeholder="Select a person"
+                        optionFilterProp="children"
+                        filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
+                    >
+                        {this.state.chapters.map(chp => <option value={chp}>{chp+1 + ' '+getChpList()[chp]}</option>)}
+                        {/* <option value="tom">Tom</option> */}
+                    </Select>
                 </form>
-                <br />
+                {/* <br />
                 <form>
                     <label htmlFor="speakerGender">Speaker Gender</label>
                     <br />
