a49c0e713cec5eb05d2c4e6b6646421e49efcb54
Yuhao Dong <1243676996@qq.com>
2022-10-10 23:52:03 -0400

    worked towards prev/next

--- src/components/PoemQuery.js
+++ src/components/PoemQuery.js
@@ -7,9 +7,12 @@ import { Link, Outlet } from 'react-router-dom';
 const { Option } = Select;
 
 export default function PoemQuery() {
+    // chapters: {0:{num: '1', count: 9, name: 'Kiritsubo 桐壺'},...}
     const [chapters, setChapters] = useState([])
-    const [chpSelect, setChpSelect] = useState([false, undefined, undefined])
+    const [chpSelect, setChpSelect] = useState([false, "", undefined])
     const [count, setCount] = useState([])
+    // prevNext: [["prevChp", "nextChp"], ["prevNum", "nextNum"]]
+    const [prevNext, setPrevNext] = useState([["",""],["",""]])
     useMemo(() => {
         let get = 'match (:Genji_Poem)-[r:INCLUDED_IN]->(c:Chapter) return c.chapter_number as num, c.chapter_name as name, count(r) as count'
         const _ = async () => {
@@ -33,7 +36,31 @@ export default function PoemQuery() {
             closeDriver()
         }
         _().catch(console.error)
+        
     }, [])
+
+    // value: int, poem order
+    function updatePrevNext(value, buttonClick) {
+        let currChp = parseInt(chpSelect[1])
+        // console.log(chpSelect[1], value)
+        if (value < count.length) {
+            setPrevNext([[chpSelect[1], value - 1],[chpSelect[1], value+1]])
+        } else if (chpSelect[1] === '1' && value === 1) {
+            setPrevNext([['1', 1],['1', 2]])
+        } else if (chpSelect[1] === '54') {
+            setPrevNext([['53', 28],['54', 1]])
+        } else if (chpSelect[1] !== '1' && value === '1') {
+            setPrevNext([[chapters[currChp - 2].num, chapters[currChp - 2].count],[chapters[currChp - 1].num, 2]])
+            // console.log("line 54")
+        } else if (value === count.length) {
+            setPrevNext([[chapters[currChp - 1].num, value - 1],[chapters[currChp].num, 1]])
+            // console.log("line 57")
+        }
+        if (buttonClick) {
+            setChpSelect([true, prevNext[1][0], prevNext[1][1]])
+            setCount(Array.from({length: chapters[parseInt(chpSelect[1]) - 1].count}, (_, i) => i + 1))
+        }
+    }
 
     return (
         <Row>
@@ -42,6 +69,7 @@ export default function PoemQuery() {
                     showSearch
                     placeholder="Select a chapter"
                     style={{ width:220 }}
+                    value={chpSelect[1]}
                     onSelect={(value) => {
                         setChpSelect([true, value, chpSelect[2]])
                         setCount(Array.from({length: chapters[value-1].count}, (_, i) => i + 1))
@@ -63,6 +91,7 @@ export default function PoemQuery() {
                     value={chpSelect[2]}
                     onSelect={(value) => {
                         setChpSelect([chpSelect[0], chpSelect[1], value])
+                        updatePrevNext(value, false)
                     }}
                 >
                     {count.map(ct => 
@@ -84,7 +113,19 @@ export default function PoemQuery() {
             <Col span={16}>
                 <Outlet />
             </Col>
-            <Col span={4}></Col>
+            <Col span={4}>
+                <Link
+                    to={`/poem/${prevNext[1][0]}/${prevNext[1][1]}`}    
+                >
+                    <Button
+                        onClick={
+                            () => {
+                                updatePrevNext(prevNext[1][1], true)
+                            }
+                        }
+                    >Next</Button>
+                </Link>
+            </Col>
         </Row>
     )
 }
