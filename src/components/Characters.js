bbff205a5a0b01398c34bd2d256c107574cef930
Yuhao Dong <1243676996@qq.com>
2023-02-22 21:32:07 -0500

    made the nodes to display

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,37 +1,14 @@
-import React, { useCallback, useEffect } from 'react';
-import ReactFlow, {
-    addEdge,
-    MiniMap,
-    Controls,
-    Background,
-    useNodesState,
-    useEdgesState,
-} from 'reactflow';
+import React, { useEffect } from 'react';
+import { ReactFlowProvider } from 'reactflow';
 import { initDriver, getDriver, closeDriver } from '../neo4j'
-// import { nodes as initialNodes, edges as initialEdges } from './initial-elements';
-import { generateGeneology, nodes as initialNodes, edges as initialEdges } from './initial-elements'
 import 'reactflow/dist/style.css';
 import { concatObj, toNativeTypes } from './utils';
 import GeneologyMap from './GeneologyMap';
-// import './overview.css';
 import { traj } from './traj'
 
-
 export default function Characters() {
-    // const nodeTypes = {
-    //     custom: CustomNode,
-    // };  
-    const minimapStyle = {
-        height: 120,
-    };
-
-    const onInit = (reactFlowInstance) => console.log('flow loaded:', reactFlowInstance);
     const [graph, setGraph] = React.useState([])
-    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    // const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-    const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
-
+    const [isLoading, setIsLoading] = React.useState(true)
     /**
      * @param {Array} l the array of edges
      */
@@ -81,44 +58,20 @@ export default function Characters() {
             const session = driver.session()
             let resGraph = await session.readTransaction(tx => tx.run('MATCH (a:Character)-[r]-(b:Character) return a.name as startName, TYPE(r) as rel, b.name as endName'))
             resGraph = resGraph.records.map(e => [concatObj(toNativeTypes(e.get('startName'))), concatObj(toNativeTypes(e.get('rel'))), concatObj(toNativeTypes(e.get('endName')))])
-            // let [nodes, edges] = generateGeneology(resGraph)
-            // setNodes(nodes)
-            // setEdges(edges)
             setGraph(generateGeneology(resGraph))
             session.close()
             closeDriver()
+            setIsLoading(false)
         }
         _().catch(console.error)
     }, [])
 
-    // we are using a bit of a shortcut here to adjust the edge type
-    // this could also be done with a custom edge for example
-    // const edgesWithUpdatedTypes = edges.map((edge) => {
-    //     if (edge.sourceHandle) {
-    //         const edgeType = nodes.find((node) => node.type === 'custom').data.selects[edge.sourceHandle];
-    //         edge.type = edgeType;
-    //     }
-
-    //     return edge;
-    // });
-
     return (
-        // <ReactFlow
-        //     nodes={nodes}
-        //     // edges={edgesWithUpdatedTypes}
-        //     edges={edges}
-        //     onNodesChange={onNodesChange}
-        //     onEdgesChange={onEdgesChange}
-        //     onConnect={onConnect}
-        //     onInit={onInit}
-        //     fitView
-        //     attributionPosition="top-right"
-        //     // nodeTypes={nodeTypes}
-        // >
-        //     <MiniMap style={minimapStyle} zoomable pannable />
-        //     <Controls />
-        //     <Background color="#aaa" gap={16} />
-        // </ReactFlow>
-        <GeneologyMap l={graph}/>
+        <>{isLoading 
+            ? null 
+            :<ReactFlowProvider>
+                <GeneologyMap l={graph}/>
+            </ReactFlowProvider>}
+        </>
     );
 };
